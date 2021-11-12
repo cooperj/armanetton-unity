cb2a1a53455267a387b343b225a1c525ef2565d9
Joshua Cooper <hi@joshc.uk>
2021-11-12 12:16:25 +0000

    :hammer: Added window spawner script and started spawning windows

--- Assets/Scripts/WindowSpawner.cs
+++ Assets/Scripts/WindowSpawner.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class WindowSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] windowTypes;
+    public Transform[] spawnPoints;
+
+    private void Start()
     {
-        
+        SpawnWindow();
+        SpawnWindow();
+        SpawnWindow();
+        SpawnWindow();
+        SpawnWindow();
+        SpawnWindow();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnWindow ()
     {
-        
+        Instantiate(GetRandomWindowType(), GetRandomSpawnPosition(), Quaternion.identity);
+    }
+
+    GameObject GetRandomWindowType()
+    {
+        int i = Random.Range(0, windowTypes.Length);
+        return windowTypes[i];
+    }
+
+    Vector3 GetRandomSpawnPosition()
+    {
+        int i = Random.Range(0, spawnPoints.Length);
+        return spawnPoints[i].position;
     }
 }
